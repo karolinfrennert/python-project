ab2af8226944d36ae9aa3c967b354f9d89dd394b
karolinfrennert <karolinfrennert@gmail.com>
2024-10-01 18:20:35 +0200

    Make result messages more fun

--- game/display_results.py
+++ game/display_results.py
@@ -1,16 +1,39 @@
 import random
 
 def display_result(player_choice, computer_choice, winner, player_wins, computer_wins):
+    emoji_dict = {
+        'rock': '🪨',
+        'paper': '📄',
+        'scissors': '✂'
+    }
     
-    print(f"\nYour choice: ({player_choice})")
-    print(f"Computer's choice: ({computer_choice})")
+    win_messages = [
+        "Awesome! You smashed it! 💥",
+        "You're on fire! 🔥",
+        "You've got the magic touch! ✨"
+    ]
+    
+    lose_messages = [
+        "Oh no, you lost. Better luck next time! 🍀",
+        "Dang! The robots are taking over 🤖",
+        "You lost... Don't give up! 💪"
+    ]
+    
+    tie_messages = [
+        "It's a tie! You can do better 😐",
+        "No winners here... at least you didn't lose! 🤙🏻",
+        "A draw...try again! ☯️"
+    ]
+    
+    print(f"\nYour choice: {emoji_dict[player_choice]} ({player_choice})")
+    print(f"Computer's choice: {emoji_dict[computer_choice]} ({computer_choice})")
 
     if winner == 'tie':
-        print('It is a tie')
+        print(random.choice(tie_messages))
     elif winner == 'player':
-        print('You win!')
+        print(random.choice(win_messages))
     else:
-        print('Computer wins :(')
+        print(random.choice(lose_messages))
 
     print(f"Score: Player: {player_wins}, Computer: {computer_wins}")
     print("\n")
